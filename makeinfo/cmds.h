45a7c4397d7d276fec18030c17716d8669464100
Karl Berry <karl@gnu.org>
2002-11-04 22:15:19 +0000

    copyright.

--- makeinfo/cmds.h
+++ makeinfo/cmds.h
@@ -1,7 +1,7 @@
 /* cmds.h -- declarations for cmds.c.
    $Id$
 
-   Copyright (C) 1998, 99 Free Software Foundation, Inc.
+   Copyright (C) 1998, 1999, 2002 Free Software Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
