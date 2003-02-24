f5b9953251c698be2df608afea6fa70847496a81
Karl Berry <karl@gnu.org>
2003-02-24 14:40:47 +0000

    (get_item_function): check for @item in argument to @itemize.  Report from: Dumas Patrice <dumas@centre-cired.fr>, 24 Feb 2003 13:13:52 +0100.

--- makeinfo/insertion.c
+++ makeinfo/insertion.c
@@ -130,7 +130,20 @@ char *
 get_item_function ()
 {
   char *item_function;
+  char *item_loc;
+  
   get_rest_of_line (0, &item_function);
+
+  /* If the document erroneously says
+       @itemize @bullet @item foobar
+     it's nicer to give an error up front than repeat `@bullet expected
+     braces' until we give up.  */
+  item_loc = strstr (item_function, "@item");
+  if (item_loc)
+    {
+      line_error (_("@item not allowed in argument to @itemize"));
+      *item_loc = 0;
+    }
 
   /* If we hit the end of text in get_rest_of_line, backing up
      input pointer will cause the last character of the last line
