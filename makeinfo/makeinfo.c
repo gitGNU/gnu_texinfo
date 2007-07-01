a0ae9d00cb80cd3300bc5cf78ca18938bf4a8813
Karl Berry <karl@gnu.org>
2007-07-01 15:35:46 +0000

    --version output

--- makeinfo/makeinfo.c
+++ makeinfo/makeinfo.c
@@ -787,13 +787,11 @@ main (int argc, char **argv)
         case 'V': /* --version */
           print_version_info ();
           puts ("");
-      printf (_("\
-Copyright (C) %s Free Software Foundation, Inc.\n\
-There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A\n\
-PARTICULAR PURPOSE.  You may redistribute copies of GNU %s\n\
-under the terms of the GNU General Public License.\n\
-For more information about these matters, see the file named COPYING.\n"),
-              "2005", PACKAGE);
+          printf (_("Copyright (C) %s Free Software Foundation, Inc.\n\
+License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n\
+This is free software: you are free to change and redistribute it.\n\
+There is NO WARRANTY, to the extent permitted by law.\n"),
+	      "2007");
           xexit (0);
           break;
 
